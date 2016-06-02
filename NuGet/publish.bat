47fec775e4f213db6eafbf9b417f43c0f194c878
artem-dudarev <ad@virtoway.com>
2016-06-02 15:11:46 +0200

    Added VirtoCommerce.CustomerModule.Client

--- NuGet/publish.bat
+++ NuGet/publish.bat
@@ -1,2 +1,4 @@
-set V=2.10.1
+set V=2.10.4
+nuget push VirtoCommerce.CustomerModule.Client.%V%.nupkg -Source nuget.org -ApiKey %1
 nuget push VirtoCommerce.CustomerModule.Data.%V%.nupkg -Source nuget.org -ApiKey %1
+pause
