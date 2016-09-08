db2105bc54b48a014ed8f943fe5c5e9d2295e809
eMazeika <em@virtoway.com>
2016-09-08 14:20:44 +0300

    Client project deleted

--- VirtoCommerce.CustomerModule.Client/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("VirtoCommerce.CustomerModule.Client")]
-[assembly: AssemblyDescription("VirtoCommerce.Customer API client generated from a Swagger doc")]
