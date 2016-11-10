56d2035daa8ff0701460c2a5205d76450b6c1fef
eMazeika <em@virtoway.com>
2016-11-10 23:54:57 +0200

    email validation fixed #1

--- VirtoCommerce.CustomerModule.Data/Model/EmailDataEntity.cs
+++ VirtoCommerce.CustomerModule.Data/Model/EmailDataEntity.cs
@@ -1,64 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Runtime.Serialization;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
 using VirtoCommerce.Platform.Core.Common;
-using VirtoCommerce.Platform.Data.Infrastructure;
 
 namespace VirtoCommerce.CustomerModule.Data.Model
 {
-		
-	public class EmailDataEntity : Entity
-	{
-		[CustomValidation(typeof(EmailDataEntity), "ValidateEmailContent", ErrorMessage = "Email has error")]
+
+    public class EmailDataEntity : Entity
+    {
+        [CustomValidation(typeof(EmailDataEntity), "ValidateEmailContent", ErrorMessage = "Email has error")]
         [StringLength(254)]
         [Index(IsUnique = false)]
-		public string Address { get; set; }
+        public string Address { get; set; }
 
-		public bool IsValidated { get; set; }
+        public bool IsValidated { get; set; }
 
-		[StringLength(64)]
-		public string Type { get; set; }
+        [StringLength(64)]
+        public string Type { get; set; }
 
 
-		#region Navigation Properties
+        #region Navigation Properties
 
-		public string MemberId { get; set; }
-       
+        public string MemberId { get; set; }
+
         public virtual MemberDataEntity Member { get; set; }
 
 
-		#endregion
+        #endregion
 
-		#region Validation
+        #region Validation
 
-		public static ValidationResult ValidateEmailContent(string value, ValidationContext context)
-		{
-			if (value == null || string.IsNullOrEmpty(value))
-			{
-				return new ValidationResult("Email can't be empty");
-			}
+        public static ValidationResult ValidateEmailContent(string value, ValidationContext context)
+        {
+            if (value == null || string.IsNullOrEmpty(value))
+            {
+                return new ValidationResult("Email can't be empty");
+            }
 
-			Regex regex = new Regex(@"^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$");
-			if (!regex.IsMatch(value))
-			{
-				return new ValidationResult((@"Email must be ""email@server.[domain 2].domain"));
-			}
-			else
-			{
-				return ValidationResult.Success;
-			}
+            Regex regex = new Regex(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
+            if (!regex.IsMatch(value))
+            {
+                return new ValidationResult((@"Email must be ""email@server.[domain 2].domain"));
+            }
+            else
+            {
+                return ValidationResult.Success;
+            }
 
-		}
+        }
 
-		#endregion
+        #endregion
 
-	}
+    }
 
 
 }
