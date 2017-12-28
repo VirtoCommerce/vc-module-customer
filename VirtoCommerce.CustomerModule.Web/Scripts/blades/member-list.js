ea461e658b587065c0d64109a760fc4f37bbe537
Andrei Kostyrin <kostyrin2@gmail.com>
2017-12-28 21:46:32 +0200

    1223 small refactor

--- VirtoCommerce.CustomerModule.Web/Scripts/blades/member-list.js
+++ VirtoCommerce.CustomerModule.Web/Scripts/blades/member-list.js
@@ -6,21 +6,18 @@ function ($scope, members, dialogService, bladeUtils, uiGridHelper, memberTypesR
     var blade = $scope.blade;
     blade.title = 'customer.blades.member-list.title';
     var bladeNavigationService = bladeUtils.bladeNavigationService;
-    var searchParams;
 
     blade.refresh = function (parentRefresh) {
         blade.isLoading = true;
-        searchParams = {
-            memberType: blade.memberType,
-            memberId: blade.currentEntity.id,
-            searchPhrase: filter.keyword ? filter.keyword : undefined,
-            deepSearch: filter.keyword ? true : false,
-            sort: uiGridHelper.getSortExpression($scope),
-            skip: ($scope.pageSettings.currentPage - 1) * $scope.pageSettings.itemsPerPageCount,
-            take: $scope.pageSettings.itemsPerPageCount
-        };
-        members.search(
-            searchParams ,
+        members.search({
+                memberType: blade.memberType,
+                memberId: blade.currentEntity.id,
+                searchPhrase: filter.keyword ? filter.keyword : undefined,
+                deepSearch: filter.keyword ? true : false,
+                sort: uiGridHelper.getSortExpression($scope),
+                skip: ($scope.pageSettings.currentPage - 1) * $scope.pageSettings.itemsPerPageCount,
+                take: $scope.pageSettings.itemsPerPageCount
+            } ,
             function (data) {
                 blade.isLoading = false;
                 $scope.pageSettings.totalItems = data.totalCount;
