2be87d43b71deb0ba37413e60624debb2b72dd9c
Kostyrin <kostyrin2@gmail.com>
2017-12-29 10:07:07 +0200

    Make bulk delete members (#10)
    * 1223 make bulk delete members
    
    * 1223 refactor - add the search in a loop
    
    * 1223 seprate search and delete functionalities
    
    * 1223 small refactor
    
    * 1223 js refactor
    
    * 1223 use var

--- VirtoCommerce.CustomerModule.Web/Scripts/resources/members.js
+++ VirtoCommerce.CustomerModule.Web/Scripts/resources/members.js
@@ -2,7 +2,8 @@
 .factory('virtoCommerce.customerModule.members', ['$resource', function ($resource) {
     return $resource('api/members/:id', {}, {
         search: { method: 'POST', url: 'api/members/search' },
-        update: { method: 'PUT' }
+        update: { method: 'PUT' },
+        delete: { method: 'POST', url: 'api/members/delete'}
     });
 }])
 .factory('virtoCommerce.customerModule.organizations', ['$resource', function ($resource) {
