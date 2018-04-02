6b842b5c5ace756a49e4cdaea147d5af64f0f28f
Eugeny Tatarincev <et@virtoway.com>
2018-04-02 10:50:46 +0200

    https://github.com/VirtoCommerce/vc-module-customer/issues/27 Display & allow to set User Groups for Organization

--- VirtoCommerce.CustomerModule.Web/Scripts/blades/organization-detail.js
+++ VirtoCommerce.CustomerModule.Web/Scripts/blades/organization-detail.js
@@ -1,5 +1,5 @@
 ﻿angular.module('virtoCommerce.customerModule')
-.controller('virtoCommerce.customerModule.organizationDetailController', ['$scope', function ($scope) {
+    .controller('virtoCommerce.customerModule.organizationDetailController', ['$scope', 'platformWebApp.settings', 'platformWebApp.bladeNavigationService', function ($scope, settings, bladeNavigationService) {
     var blade = $scope.blade;
 
     if (blade.isNew) {
@@ -10,4 +10,17 @@
     } else {
         blade.subtitle = 'customer.blades.organization-detail.subtitle';
     }
+    $scope.groups = settings.getValues({ id: 'Customer.MemberGroups' });
+    $scope.openGroupsDictionarySettingManagement = function () {
+        var newBlade = {
+            id: 'settingDetailChild',
+            isApiSave: true,
+            currentEntityId: 'Customer.MemberGroups',
+            parentRefresh: function (data) { $scope.groups = data; },
+            controller: 'platformWebApp.settingDictionaryController',
+            template: '$(Platform)/Scripts/app/settings/blades/setting-dictionary.tpl.html'
+        };
+        bladeNavigationService.showBlade(newBlade, blade);
+    };
+
 }]);
