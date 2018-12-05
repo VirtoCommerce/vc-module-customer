d9ac67b39e6dfc2282acd2f402af8f05c449fe0f
Mikhail Smal <mvsmal@gmail.com>
2018-12-05 15:21:57 +0100

    Implement Member Changes event handler

---
+++ VirtoCommerce.CustomerModule.Data/Handlers/LogChangesMemberChangedEventHandler.cs
@@ -0,0 +1,248 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using AutoCompare;
+using VirtoCommerce.Domain.Commerce.Model;
+using VirtoCommerce.Domain.Common.Events;
+using VirtoCommerce.Domain.Customer.Events;
+using VirtoCommerce.Domain.Customer.Model;
+using VirtoCommerce.Domain.Customer.Services;
+using VirtoCommerce.Domain.Order.Model;
+using VirtoCommerce.Platform.Core.ChangeLog;
+using VirtoCommerce.Platform.Core.Common;
+using VirtoCommerce.Platform.Core.Events;
+
+namespace VirtoCommerce.CustomerModule.Data.Handlers
+{
+    public class LogChangesMemberChangedEventHandler : IEventHandler<MemberChangedEvent>
+    {
+        private readonly IChangeLogService _changeLogService;
+        private static readonly string[] ObservedProperties;
+
+        static LogChangesMemberChangedEventHandler()
+        {
+            var memberPropsNames = ReflectionUtility.GetPropertyNames<Contact>(
+                x => x.Name, x => x.FirstName, x => x.LastName, x => x.MiddleName,
+                x => x.Salutation, x => x.FullName, x => x.BirthDate);
+            ObservedProperties = memberPropsNames.ToArray();
+        }
+
+        public LogChangesMemberChangedEventHandler(IChangeLogService changeLogService)
+        {
+            _changeLogService = changeLogService;
+        }
+
+        public virtual Task Handle(MemberChangedEvent message)
+        {
+            var operationLogs =
+                message.ChangedEntries
+                    .Where(x => x.EntryState == EntryState.Modified)
+                    .SelectMany(GetChangedEntryOperationLogs);
+            _changeLogService.SaveChanges(operationLogs.ToArray());
+            return Task.CompletedTask;
+        }
+
+        protected virtual IEnumerable<OperationLog> GetChangedEntryOperationLogs(
+            GenericChangedEntry<Member> changedEntry)
+        {
+            var result = new List<string>();
+
+            var diff = Comparer.Compare(changedEntry.OldEntry, changedEntry.NewEntry);
+
+            if (changedEntry.OldEntry is Contact contact)
+            {
+                result.AddRange(GetContactChanges(contact, changedEntry.NewEntry as Contact));
+                diff.AddRange(Comparer.Compare(contact, changedEntry.NewEntry as Contact));
+            }
+
+            var observedDifferences =
+                diff.Join(ObservedProperties, x => x.Name.ToLowerInvariant(), x => x.ToLowerInvariant(), (x, y) => x)
+                    .ToArray();
+            foreach (var difference in observedDifferences.Distinct(new DifferenceComparer()))
+            {
+                AddMemberPropertyChangedOperation(result, changedEntry, difference);
+            }
+
+            return result.Select(x => GetLogRecord(changedEntry.NewEntry, x));
+        }
+
+        protected virtual IEnumerable<string> GetContactChanges(Contact originalContact, Contact modifiedContact)
+        {
+            return
+                GetAddressChanges(
+                    originalContact, originalContact.Addresses, modifiedContact.Addresses)
+                    .Union(GetEmailChanges(originalContact, originalContact.Emails, modifiedContact.Emails))
+                    .Union(GetPhoneChanges(originalContact, originalContact.Phones, modifiedContact.Phones));
+        }
+
+        protected virtual IEnumerable<string> GetPhoneChanges(
+            Member member, IEnumerable<string> originalPhones, IEnumerable<string> modifiedPhones)
+        {
+            return GetListChanges(
+                originalPhones,
+                modifiedPhones,
+                result =>
+                    (state, source, target) =>
+                    {
+                        if (state == EntryState.Added)
+                        {
+                            AddPhoneAddedOperation(result, member, target);
+                        }
+                        else if (state == EntryState.Deleted)
+                        {
+                            AddPhoneRemovedOperation(result, member, target);
+                        }
+                    });
+        }
+
+        protected virtual IEnumerable<string> GetEmailChanges(
+            Member member, IEnumerable<string> originalEmails, IEnumerable<string> modifiedEmails)
+        {
+            return GetListChanges(
+                originalEmails,
+                modifiedEmails,
+                result =>
+                    (state, source, target) =>
+                    {
+                        if (state == EntryState.Added)
+                        {
+                            AddEmailAddedOperation(result, member, target);
+                        }
+                        else if (state == EntryState.Deleted)
+                        {
+                            AddEmailRemovedOperation(result, member, target);
+                        }
+                    });
+        }
+
+        protected virtual IEnumerable<string> GetAddressChanges(
+            Member member, IEnumerable<Address> originalAddress, IEnumerable<Address> modifiedAddress)
+        {
+            return GetListChanges(
+                originalAddress,
+                modifiedAddress,
+                result =>
+                    (state, source, target) =>
+                    {
+                        if (state == EntryState.Added)
+                        {
+                            AddAddressAddedOperation(result, member, target);
+                        }
+                        else if (state == EntryState.Deleted)
+                        {
+                            AddAddressRemovedOperation(result, member, target);
+                        }
+                    });
+        }
+
+        protected virtual IEnumerable<string> GetListChanges<T>(
+            IEnumerable<T> originalCollection,
+            IEnumerable<T> modifiedCollection,
+            Func<List<string>, Action<EntryState, T, T>> compareAction)
+        {
+            if (originalCollection == null || modifiedCollection == null)
+            {
+                return Enumerable.Empty<string>();
+            }
+
+            var result = new List<string>();
+            modifiedCollection
+                .Where(x => x != null).ToList()
+                .CompareTo(
+                    originalCollection.Where(x => x != null).ToList(),
+                    EqualityComparer<T>.Default,
+                    compareAction(result));
+            return result;
+        }
+
+        private static void AddMemberPropertyChangedOperation(
+            List<string> result, GenericChangedEntry<Member> changedEntry, Difference difference)
+        {
+            result.Add(
+                string.Format(
+                    MemberResources.MemberPropertyChanged,
+                    changedEntry.OldEntry.MemberType,
+                    changedEntry.NewEntry.Name,
+                    difference.Name,
+                    difference.OldValue,
+                    difference.NewValue));
+        }
+
+        private void AddPhoneRemovedOperation(List<string> result, Member member, string phone)
+        {
+            result.Add(string.Format(MemberResources.PhoneRemoved, member.MemberType, member.Name, phone));
+        }
+
+        private void AddPhoneAddedOperation(List<string> result, Member member, string phone)
+        {
+            result.Add(string.Format(MemberResources.PhoneAdded, member.MemberType, member.Name, phone));
+        }
+
+        private void AddEmailRemovedOperation(List<string> result, Member member, string email)
+        {
+            result.Add(string.Format(MemberResources.EmailRemoved, member.MemberType, member.Name, email));
+        }
+
+        private void AddEmailAddedOperation(List<string> result, Member member, string email)
+        {
+            result.Add(string.Format(MemberResources.EmailAdded, member.MemberType, member.Name, email));
+        }
+
+        private void AddAddressRemovedOperation(List<string> result, Member member, Address address)
+        {
+            result.Add(
+                string.Format(MemberResources.AddressRemoved, member.MemberType, member.Name, StringifyAddress(address)));
+        }
+
+        private void AddAddressAddedOperation(List<string> result, Member member, Address address)
+        {
+            result.Add(
+                string.Format(MemberResources.AddressAdded, member.MemberType, member.Name, StringifyAddress(address)));
+        }
+
+        private string StringifyAddress(Address address)
+        {
+            var result = "";
+            if (address != null)
+            {
+                return string.Join(
+                    ", ",
+                    typeof(Address)
+                        .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                        .OrderBy(p => p.Name)
+                        .Select(p => p.GetValue(address))
+                        .Where(x => x != null));
+            }
+            return result;
+        }
+
+        protected virtual OperationLog GetLogRecord(Member member, string template)
+        {
+            var result = new OperationLog
+            {
+                ObjectId = member.Id,
+                ObjectType = member.GetType().Name,
+                OperationType = EntryState.Modified,
+                Detail = template
+            };
+            return result;
+
+        }
+    }
+
+    internal class DifferenceComparer : EqualityComparer<Difference>
+    {
+        public override bool Equals(Difference x, Difference y)
+        {
+            return GetHashCode(x) == GetHashCode(y);
+        }
+
+        public override int GetHashCode(Difference obj)
+        {
+            var result = String.Join(":", obj.Name, obj.NewValue, obj.OldValue);
+            return result.GetHashCode();
+        }
+    }
+}
