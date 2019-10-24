96da0196200a51de02fe081e678564686a95d15a
Andrey Kostyrin <kostyrin2@gmail.com>
2019-10-24 17:24:08 +0200

    #vp-808 Event based indexation (#63)
    * #vp-808 Event based indexation
    
    * Revert "#vp-808 Event based indexation"
    
    This reverts commit 44da4760a1b4f5e0ef1d84817c5fb867c397b4ba.
    
    * #vp-808 Event based indexation refactoring
    
    * #vp-808 revert manifest

--- VirtoCommerce.CustomerModule.Web/Module.cs
+++ VirtoCommerce.CustomerModule.Web/Module.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Web.Http;
 using Microsoft.Practices.Unity;
 using VirtoCommerce.CustomerModule.Data.Handlers;
@@ -58,6 +58,10 @@ namespace VirtoCommerce.CustomerModule.Web
             eventHandlerRegistrar.RegisterHandler<MemberChangedEvent>(
                 async (message, token) =>
                     await _container.Resolve<LogChangesMemberChangedEventHandler>().Handle(message));
+
+            eventHandlerRegistrar.RegisterHandler<MemberChangedEvent>(
+               async (message, token) =>
+                   await _container.Resolve<IndexMemberChangedEventHandler>().Handle(message));
 
 
             Func<CustomerRepositoryImpl> customerRepositoryFactory = () => new CustomerRepositoryImpl(_connectionString, new EntityPrimaryKeyGeneratorInterceptor(), _container.Resolve<AuditableInterceptor>(),
