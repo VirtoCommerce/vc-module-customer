6ca705560c5b3db8eaf07a458f934f2bb3fc5bbf
Egidijus Mazeika <em@virtoway.com>
2020-05-19 22:18:27 +0300

    Apply EventBasedIndexation setting value immediately VP-2399

--- src/VirtoCommerce.CustomerModule.Data/Handlers/IndexMemberChangedEventHandler.cs
+++ src/VirtoCommerce.CustomerModule.Data/Handlers/IndexMemberChangedEventHandler.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using VirtoCommerce.CustomerModule.Core.Events;
 using VirtoCommerce.Platform.Core.Events;
+using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.SearchModule.Core.Model;
 using VirtoCommerce.SearchModule.Data.BackgroundJobs;
 
@@ -10,18 +11,28 @@ namespace VirtoCommerce.CustomerModule.Data.Handlers
 {
     public class IndexMemberChangedEventHandler : IEventHandler<MemberChangedEvent>
     {
+        private readonly ISettingsManager _settingsManager;
+
+        public IndexMemberChangedEventHandler(ISettingsManager settingsManager)
+        {
+            _settingsManager = settingsManager;
+        }
+
         public Task Handle(MemberChangedEvent message)
         {
-            if (message == null)
+            if (_settingsManager.GetValue(Core.ModuleConstants.Settings.General.EventBasedIndexation.Name, false))
             {
-                throw new ArgumentNullException(nameof(message));
+                if (message == null)
+                {
+                    throw new ArgumentNullException(nameof(message));
+                }
+
+                var indexEntries = message.ChangedEntries
+                    .Select(x => new IndexEntry { Id = x.OldEntry.Id, EntryState = x.EntryState, Type = KnownDocumentTypes.Member })
+                    .ToArray();
+
+                IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries);
             }
-
-            var indexEntries = message.ChangedEntries
-                .Select(x => new IndexEntry { Id = x.OldEntry.Id, EntryState = x.EntryState, Type = KnownDocumentTypes.Member })
-                .ToArray();
-
-            IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries);
 
             return Task.CompletedTask;
         }
