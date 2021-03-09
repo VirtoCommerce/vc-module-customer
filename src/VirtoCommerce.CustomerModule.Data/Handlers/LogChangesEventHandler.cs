94145b817ec56612594c693b5d24d69d7a302286
Egidijus Mazeika <em@virtoway.com>
2021-03-09 15:46:50 +0200

    PT-201: Write “Member“ to OperationLog.ObjectType

--- src/VirtoCommerce.CustomerModule.Data/Handlers/LogChangesEventHandler.cs
+++ src/VirtoCommerce.CustomerModule.Data/Handlers/LogChangesEventHandler.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Hangfire;
 using VirtoCommerce.CustomerModule.Core.Events;
+using VirtoCommerce.CustomerModule.Core.Model;
 using VirtoCommerce.Platform.Core.ChangeLog;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Events;
@@ -25,18 +26,25 @@ namespace VirtoCommerce.CustomerModule.Data.Handlers
 
         protected virtual void InnerHandle<T>(GenericChangedEntryEvent<T> @event) where T : IEntity
         {
-            var logOperations = @event.ChangedEntries.Select(x => AbstractTypeFactory<OperationLog>.TryCreateInstance().FromChangedEntry(x)).ToArray();
+            // TODO: after next platform ver. released, use .FromChangedEntry(x, nameof(Member)) override
+            var logOperations = @event.ChangedEntries.Select(x =>
+            {
+                var result = AbstractTypeFactory<OperationLog>.TryCreateInstance().FromChangedEntry(x);
+                result.ObjectType = nameof(Member);
+                return result;
+            }).ToArray();
+
             //Background task is used here for performance reasons
             BackgroundJob.Enqueue(() => LogEntityChangesInBackground(logOperations));
         }
 
         [DisableConcurrentExecution(10)]
         // "DisableConcurrentExecutionAttribute" prevents to start simultaneous job payloads.
-	// Should have short timeout, because this attribute implemented by following manner: newly started job falls into "processing" state immediately.
+        // Should have short timeout, because this attribute implemented by following manner: newly started job falls into "processing" state immediately.
         // Then it tries to receive job lock during timeout. If the lock received, the job starts payload.
         // When the job is awaiting desired timeout for lock release, it stucks in "processing" anyway. (Therefore, you should not to set long timeouts (like 24*60*60), this will cause a lot of stucked jobs and performance degradation.)
         // Then, if timeout is over and the lock NOT acquired, the job falls into "scheduled" state (this is default fail-retry scenario).
-	// Failed job goes to "Failed" state (by default) after retries exhausted.
+        // Failed job goes to "Failed" state (by default) after retries exhausted.
         public void LogEntityChangesInBackground(OperationLog[] operationLogs)
         {
             _changeLogService.SaveChangesAsync(operationLogs).GetAwaiter().GetResult();
