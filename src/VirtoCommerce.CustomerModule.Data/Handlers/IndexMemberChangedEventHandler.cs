2fb4aa13bab880550ef8f7f7285f2259bb8262eb
Getmansky <mge@virtoway.com>
2022-02-15 11:51:03 +0300

    Partial index update support

--- src/VirtoCommerce.CustomerModule.Data/Handlers/IndexMemberChangedEventHandler.cs
+++ src/VirtoCommerce.CustomerModule.Data/Handlers/IndexMemberChangedEventHandler.cs
@@ -1,15 +1,26 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using VirtoCommerce.CustomerModule.Core.Events;
+using VirtoCommerce.CustomerModule.Data.Search.Indexing;
 using VirtoCommerce.Platform.Core.Events;
+using VirtoCommerce.Platform.Core.Jobs;
 using VirtoCommerce.SearchModule.Core.Model;
 using VirtoCommerce.SearchModule.Data.BackgroundJobs;
+using VirtoCommerce.SearchModule.Data.Services;
 
 namespace VirtoCommerce.CustomerModule.Data.Handlers
 {
     public class IndexMemberChangedEventHandler : IEventHandler<MemberChangedEvent>
     {
+        private readonly IEnumerable<IndexDocumentConfiguration> _configurations;
+
+        public IndexMemberChangedEventHandler(IEnumerable<IndexDocumentConfiguration> configurations)
+        {
+            _configurations = configurations;
+        }
+
         public Task Handle(MemberChangedEvent message)
         {
             if (message == null)
@@ -21,7 +32,8 @@ namespace VirtoCommerce.CustomerModule.Data.Handlers
                 .Select(x => new IndexEntry { Id = x.OldEntry.Id, EntryState = x.EntryState, Type = KnownDocumentTypes.Member })
                 .ToArray();
 
-            IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries);
+            IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries,
+                JobPriority.Normal, _configurations.GetBuildersForProvider(typeof(MemberDocumentChangesProvider)).ToList());
 
             return Task.CompletedTask;
         }
