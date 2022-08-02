7de73ae7f3b173b0fd0bcd2a0ff0b7b3b17ba034
Andalexshap <andalexshap@yandex.ru>
2022-08-02 14:44:35 +0300

    CountryName and RegionName Set Not Requared

---
+++ src/VirtoCommerce.CustomerModule.Data/Migrations/20220802111623_SetCountryNameNotRequared.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtoCommerce.CustomerModule.Data.Migrations
+{
+    public partial class SetCountryNameNotRequared : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CountryName",
+                table: "Address",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CountryName",
+                table: "Address",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128,
+                oldNullable: true);
+        }
+    }
+}
