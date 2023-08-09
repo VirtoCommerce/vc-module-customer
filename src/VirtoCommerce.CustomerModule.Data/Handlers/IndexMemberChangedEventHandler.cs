0828c32a72b04ba373a55f2e389d614aef45f66f
Evgeny Okhrimenko <eo@virtoway.com>
2023-08-09 21:54:37 +0200

    Add handlers registration for user events

--- src/VirtoCommerce.CustomerModule.Data/Handlers/IndexMemberChangedEventHandler.cs
+++ src/VirtoCommerce.CustomerModule.Data/Handlers/IndexMemberChangedEventHandler.cs
@@ -4,15 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using VirtoCommerce.CustomerModule.Core.Events;
 using VirtoCommerce.CustomerModule.Data.Search.Indexing;
+using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Events;
 using VirtoCommerce.Platform.Core.Jobs;
+using VirtoCommerce.Platform.Core.Security.Events;
 using VirtoCommerce.SearchModule.Core.BackgroundJobs;
 using VirtoCommerce.SearchModule.Core.Extensions;
 using VirtoCommerce.SearchModule.Core.Model;
 
 namespace VirtoCommerce.CustomerModule.Data.Handlers
 {
-    public class IndexMemberChangedEventHandler : IEventHandler<MemberChangedEvent>
+    public class IndexMemberChangedEventHandler : IEventHandler<MemberChangedEvent>, IEventHandler<UserChangedEvent>, IEventHandler<UserRoleAddedEvent>, IEventHandler<UserRoleRemovedEvent>
     {
         private readonly IIndexingJobService _indexingJobService;
         private readonly IEnumerable<IndexDocumentConfiguration> _configurations;
@@ -25,19 +27,70 @@ namespace VirtoCommerce.CustomerModule.Data.Handlers
 
         public Task Handle(MemberChangedEvent message)
         {
+            return InnerHandle(message);
+        }
+
+        public Task Handle(UserChangedEvent message)
+        {
+            return InnerHandle(message);
+        }
+
+        public Task Handle(UserRoleAddedEvent message)
+        {
+            return InnerHandle(message);
+        }
+
+        public Task Handle(UserRoleRemovedEvent message)
+        {
+            return InnerHandle(message);
+        }
+
+        protected virtual Task InnerHandle(DomainEvent message)
+        {
             if (message == null)
             {
                 throw new ArgumentNullException(nameof(message));
             }
 
-            var indexEntries = message.ChangedEntries
-                .Select(x => new IndexEntry { Id = x.OldEntry.Id, EntryState = x.EntryState, Type = KnownDocumentTypes.Member })
-                .ToArray();
+            var indexEntries = new List<IndexEntry>();
+
+            switch (message)
+            {
+                case MemberChangedEvent memberChangedEvent:
+                    indexEntries = memberChangedEvent.ChangedEntries
+                        .Select(x => new IndexEntry { Id = x.OldEntry.Id, EntryState = x.EntryState, Type = KnownDocumentTypes.Member })
+                        .ToList();
+                    break;
+                case UserChangedEvent userChangedEvent:
+                    indexEntries = userChangedEvent.ChangedEntries
+                        .Where(x => !string.IsNullOrEmpty(x.OldEntry.MemberId))
+                        .Select(x => new IndexEntry { Id = x.OldEntry.MemberId, EntryState = x.EntryState, Type = KnownDocumentTypes.Member })
+                        .ToList();
+                    break;
+                case UserRoleAddedEvent userRoleAddedEvent when !string.IsNullOrEmpty(userRoleAddedEvent.User.MemberId):
+                    indexEntries.Add(GetIndexEntry(userRoleAddedEvent.User.MemberId));
+                    break;
+                case UserRoleRemovedEvent userRoleRemovedEvent when !string.IsNullOrEmpty(userRoleRemovedEvent.User.MemberId):
+                    indexEntries.Add(GetIndexEntry(userRoleRemovedEvent.User.MemberId));
+                    break;
+            }
 
             _indexingJobService.EnqueueIndexAndDeleteDocuments(indexEntries, JobPriority.Normal,
                 _configurations.GetDocumentBuilders(KnownDocumentTypes.Member, typeof(MemberDocumentChangesProvider)).ToList());
 
             return Task.CompletedTask;
+        }
+
+        protected virtual IndexEntry GetIndexEntry(string objectId)
+        {
+            var result = new IndexEntry
+            {
+                Id = objectId,
+                EntryState = EntryState.Modified,
+                Type = KnownDocumentTypes.Member,
+            };
+
+            return result;
         }
     }
 }
--- src/VirtoCommerce.CustomerModule.Data/Handlers/LogChangesEventHandler.cs
+++ src/VirtoCommerce.CustomerModule.Data/Handlers/LogChangesEventHandler.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Hangfire;
@@ -6,10 +8,11 @@ using VirtoCommerce.CustomerModule.Core.Model;
 using VirtoCommerce.Platform.Core.ChangeLog;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Events;
+using VirtoCommerce.Platform.Core.Security.Events;
 
 namespace VirtoCommerce.CustomerModule.Data.Handlers
 {
-    public class LogChangesEventHandler : IEventHandler<MemberChangedEvent>
+    public class LogChangesEventHandler : IEventHandler<MemberChangedEvent>, IEventHandler<UserChangedEvent>, IEventHandler<UserRoleAddedEvent>, IEventHandler<UserRoleRemovedEvent>
     {
         private readonly IChangeLogService _changeLogService;
 
@@ -24,12 +27,19 @@ namespace VirtoCommerce.CustomerModule.Data.Handlers
             return Task.CompletedTask;
         }
 
-        protected virtual void InnerHandle<T>(GenericChangedEntryEvent<T> @event) where T : IEntity
+        public virtual Task Handle(UserChangedEvent message)
         {
-            // ObjectType has to be 'Member' as MemberDocumentChangesProvider uses it to get all changed members in 1 request.
-            var logOperations = @event.ChangedEntries.Select(x => AbstractTypeFactory<OperationLog>.TryCreateInstance().FromChangedEntry(x, nameof(Member))).ToArray();
-            //Background task is used here for performance reasons
-            BackgroundJob.Enqueue(() => LogEntityChangesInBackground(logOperations));
+            return InnerUserEventHandle(message);
+        }
+
+        public virtual Task Handle(UserRoleAddedEvent message)
+        {
+            return InnerUserEventHandle(message);
+        }
+
+        public virtual Task Handle(UserRoleRemovedEvent message)
+        {
+            return InnerUserEventHandle(message);
         }
 
         [DisableConcurrentExecution(10)]
@@ -39,9 +49,60 @@ namespace VirtoCommerce.CustomerModule.Data.Handlers
         // When the job is awaiting desired timeout for lock release, it stucks in "processing" anyway. (Therefore, you should not to set long timeouts (like 24*60*60), this will cause a lot of stucked jobs and performance degradation.)
         // Then, if timeout is over and the lock NOT acquired, the job falls into "scheduled" state (this is default fail-retry scenario).
         // Failed job goes to "Failed" state (by default) after retries exhausted.
-        public void LogEntityChangesInBackground(OperationLog[] operationLogs)
+        public void LogEntityChangesInBackground(IList<OperationLog> operationLogs)
         {
-            _changeLogService.SaveChangesAsync(operationLogs).GetAwaiter().GetResult();
+            _changeLogService.SaveChangesAsync(operationLogs.ToArray()).GetAwaiter().GetResult();
+        }
+
+
+        protected virtual void InnerHandle<T>(GenericChangedEntryEvent<T> @event) where T : IEntity
+        {
+            // ObjectType has to be 'Member' as MemberDocumentChangesProvider uses it to get all changed members in 1 request.
+            var logOperations = @event.ChangedEntries.Select(x => AbstractTypeFactory<OperationLog>.TryCreateInstance().FromChangedEntry(x, nameof(Member))).ToList();
+            //Background task is used here for performance reasons
+            BackgroundJob.Enqueue(() => LogEntityChangesInBackground(logOperations));
+        }
+
+        protected virtual Task InnerUserEventHandle(DomainEvent message)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            var operationLogs = new List<OperationLog>();
+
+            switch (message)
+            {
+                case UserChangedEvent userChangedEvent:
+                    operationLogs = userChangedEvent.ChangedEntries
+                        .Where(x => !string.IsNullOrEmpty(x.OldEntry.MemberId))
+                        .Select(x => GetOperationLog(x.OldEntry.MemberId))
+                        .ToList();
+                    break;
+                case UserRoleAddedEvent userRoleAddedEvent when !string.IsNullOrEmpty(userRoleAddedEvent.User.MemberId):
+                    operationLogs.Add(GetOperationLog(userRoleAddedEvent.User.MemberId));
+                    break;
+                case UserRoleRemovedEvent userRoleRemovedEvent when !string.IsNullOrEmpty(userRoleRemovedEvent.User.MemberId):
+                    operationLogs.Add(GetOperationLog(userRoleRemovedEvent.User.MemberId));
+                    break;
+            }
+
+            BackgroundJob.Enqueue(() => LogEntityChangesInBackground(operationLogs));
+
+            return Task.CompletedTask;
+        }
+
+        protected virtual OperationLog GetOperationLog(string objectId)
+        {
+            var result = new OperationLog
+            {
+                ObjectId = objectId,
+                ObjectType = nameof(Member),
+                OperationType = EntryState.Modified,
+            };
+
+            return result;
         }
     }
 }
