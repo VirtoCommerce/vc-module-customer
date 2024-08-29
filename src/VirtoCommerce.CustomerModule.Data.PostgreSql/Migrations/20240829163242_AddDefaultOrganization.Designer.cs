c048eb7876111260999c3508c4b64517e107dab2
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-08-29 18:38:35 +0200

    VCST-1402: add DefaultOrganizationId migrations (#250)

--- src/VirtoCommerce.CustomerModule.Data.PostgreSql/Migrations/20240829163242_AddDefaultOrganization.Designer.cs
+++ src/VirtoCommerce.CustomerModule.Data.PostgreSql/Migrations/20240829163242_AddDefaultOrganization.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using VirtoCommerce.CustomerModule.Data.Repositories;
@@ -11,9 +12,11 @@ using VirtoCommerce.CustomerModule.Data.Repositories;
 namespace VirtoCommerce.CustomerModule.Data.PostgreSql.Migrations
 {
     [DbContext(typeof(CustomerDbContext))]
-    partial class CustomerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240829163242_AddDefaultOrganization")]
+    partial class AddDefaultOrganization
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
