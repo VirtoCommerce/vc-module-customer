e1817ef50e85134aedb994574b7ad65256b76db8
Artem Dudarev <artem@virtoworks.com>
2024-12-12 16:47:19 +0200

    Refactoring

--- src/VirtoCommerce.CustomerModule.Web/Scripts/blades/pick-security-account.js
+++ src/VirtoCommerce.CustomerModule.Web/Scripts/blades/pick-security-account.js
@@ -4,116 +4,130 @@ angular.module('virtoCommerce.customerModule')
             'platformWebApp.bladeNavigationService', 'platformWebApp.bladeUtils',
             'virtoCommerce.customerModule.members', 'platformWebApp.dialogService',
             function ($scope, accounts, uiGridHelper, bladeNavigationService, bladeUtils, members, dialogService) {
-    $scope.uiGridConstants = uiGridHelper.uiGridConstants;
-    var blade = $scope.blade;
-    var filter = $scope.filter = {};
+                var blade = $scope.blade;
+                blade.headIcon = 'fas fa-key';
+                blade.title = 'customer.blades.pick-security-account-list.title';
+                blade.subtitle = 'customer.blades.pick-security-account-list.subtitle';
 
+                $scope.uiGridConstants = uiGridHelper.uiGridConstants;
+                var filter = $scope.filter = {};
 
-    blade.title = 'customer.blades.pick-security-account-list.title';
-    blade.subtitle = 'customer.blades.pick-security-account-list.subtitle';
+                blade.refresh = function () {
+                    blade.isLoading = true;
 
-    blade.refresh = function () {
-        blade.isLoading = true;
+                    accounts.search({
+                        keyword: filter.keyword,
+                        sort: uiGridHelper.getSortExpression($scope),
+                        skip: ($scope.pageSettings.currentPage - 1) * $scope.pageSettings.itemsPerPageCount,
+                        take: $scope.pageSettings.itemsPerPageCount
+                    }, function (data) {
+                        blade.isLoading = false;
 
-        accounts.search({
-            keyword: filter.keyword,
-            sort: uiGridHelper.getSortExpression($scope),
-            skip: ($scope.pageSettings.currentPage - 1) * $scope.pageSettings.itemsPerPageCount,
-            take: $scope.pageSettings.itemsPerPageCount
-        }, function (data) {
-            blade.isLoading = false;
+                        $scope.pageSettings.totalItems = data.totalCount;
+                        blade.currentEntities = data.results;
+                    }, function (error) {
+                        bladeNavigationService.setError('Error ' + error.status, blade);
+                    });
+                };
 
-            $scope.pageSettings.totalItems = data.totalCount;
-            blade.currentEntities = data.results;
-        }, function (error) {
-            bladeNavigationService.setError('Error ' + error.status, blade);
-        });
-    };
+                blade.selectNode = function (node) {
+                    $scope.selectedNodeId = node.id;
 
-    blade.selectNode = function (node) {
-        const nodeMemberId = node.memberId;
+                    var newBlade = {
+                        id: 'listItemChild',
+                        controller: 'platformWebApp.accountDetailController',
+                        template: '$(Platform)/Scripts/app/security/blades/account-detail.tpl.html',
+                        data: node,
+                        title: node.userName,
+                        subtitle: blade.subtitle,
+                    };
 
-        if (nodeMemberId) {
-            members.get({ id: nodeMemberId }, function (member) {
-                if (member?.name) {
-                    const dialog = {
-                        id: 'confirmLinkAccount',
-                        title: 'customer.blades.pick-security-account-list.title',
-                        message: `The selected account is already associated with ${member.name}. Do you want to proceed?`,
-                        callback: function (link) {
-                            if (link) {
-                                $scope.gridApi.selection.selectRow(node);
-                            } else {
-                                $scope.gridApi.selection.unSelectRow(node);
+                    bladeNavigationService.showBlade(newBlade, blade);
+                };
+
+                function isRowSelectable(row) {
+                    return row.entity.memberId !== blade.currentEntity.memberId;
+                }
+
+                function rowSelectionChanged(row) {
+                    if (row.isSelected && row.entity.memberId) {
+                        members.get({ id: row.entity.memberId }, function (member) {
+                            if (member) {
+                                const dialog = {
+                                    id: 'confirmLinkAccount',
+                                    title: 'customer.dialogs.confirm-account-link.title',
+                                    message: 'customer.dialogs.confirm-account-link.message',
+                                    messageValues: { memberName: member.name },
+                                    callback: function (isConfirmed) {
+                                        row.isSelected = isConfirmed;
+                                    },
+                                };
+                                dialogService.showConfirmationDialog(dialog);
                             }
+                        });
+                    }
+                }
+
+                function linkAccounts(selectedAccounts) {
+                    blade.isLoading = true;
+
+                    const updatePromises = selectedAccounts.map(account => {
+                        return new Promise((resolve, reject) => {
+                            account.memberId = blade.currentEntity.memberId;
+                            accounts.update(account, function (result) {
+                                if (result.succeeded) {
+                                    resolve();
+                                } else {
+                                    bladeNavigationService.setError(result.errors.join(), blade);
+                                    reject(result.errors);
+                                }
+                            });
+                        });
+                    });
+
+                    Promise.all(updatePromises)
+                        .then(() => {
+                            blade.parentBlade.refresh();
+                            $scope.bladeClose();
+                        })
+                        .finally(() => {
+                            blade.isLoading = false;
+                        });
+                }
+
+                blade.toolbarCommands = [
+                    {
+                        name: "platform.commands.save", icon: 'fas fa-save',
+                        executeMethod: function () { linkAccounts($scope.gridApi.selection.getSelectedRows()); },
+                        canExecuteMethod: () => $scope.gridApi?.selection?.getSelectedRows()?.length > 0
+                    },
+                    {
+                        name: "platform.commands.refresh", icon: 'fa fa-refresh',
+                        executeMethod: blade.refresh,
+                        canExecuteMethod: function () {
+                            return true;
                         }
                     }
-                    dialogService.showConfirmationDialog(dialog);
-                }
-                else {
-                    $scope.gridApi.selection.selectRow(node);
-                }
-            });
-        }
-    };
+                ];
 
-
-    blade.linkAccount = function () {
-        blade.isLoading = true;
-
-        const selectedNodes = $scope.gridApi.selection.getSelectedRows();
-        const updatePromises = selectedNodes.map(node => {
-            return new Promise((resolve, reject) => {
-                node.memberId = blade.currentEntity.memberId;
-                accounts.update(node, function (result) {
-                    if (result.succeeded) {
-                        resolve();
+                filter.criteriaChanged = function () {
+                    if ($scope.pageSettings.currentPage > 1) {
+                        $scope.pageSettings.currentPage = 1;
                     } else {
-                        bladeNavigationService.setError(result.errors.join(), blade);
-                        reject(result.errors);
+                        blade.refresh();
                     }
-                });
-            });
-        });
+                };
 
-        Promise.all(updatePromises).then(() => {
-            blade.parentBlade.refresh();
-            $scope.bladeClose();
-        }).finally(() => {
-            blade.isLoading = false;
-        });
-    };
+                // ui-grid
+                $scope.setGridOptions = function (gridOptions) {
+                    gridOptions.enableSelectAll = false;    
+                    gridOptions.isRowSelectable = isRowSelectable;
 
-    blade.headIcon = 'fas fa-key';
+                    uiGridHelper.initialize($scope, gridOptions, function (gridApi) {
+                        uiGridHelper.bindRefreshOnSortChanged($scope);
+                        gridApi.selection.on.rowSelectionChanged($scope, rowSelectionChanged);
+                    });
 
-    blade.toolbarCommands = [
-        {
-            name: "platform.commands.save", icon: 'fas fa-save',
-            executeMethod: blade.linkAccount,
-            canExecuteMethod: () => $scope.gridApi?.selection?.getSelectedRows()?.length > 0
-        },
-        {
-            name: "platform.commands.refresh", icon: 'fa fa-refresh',
-            executeMethod: blade.refresh,
-            canExecuteMethod: function () {
-                return true;
-            }
-        }
-    ];
-
-    filter.criteriaChanged = function () {
-        if ($scope.pageSettings.currentPage > 1) {
-            $scope.pageSettings.currentPage = 1;
-        } else {
-            blade.refresh();
-        }
-    };
-
-    // ui-grid
-    $scope.setGridOptions = function (gridOptions) {
-        uiGridHelper.initialize($scope, gridOptions, function (gridApi) {
-            uiGridHelper.bindRefreshOnSortChanged($scope);
-        });
-        bladeUtils.initializePagination($scope);
-    };
-}]);
+                    bladeUtils.initializePagination($scope);
+                };
+            }]);
