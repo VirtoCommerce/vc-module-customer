7732f2d96f1dd76ce4e67037018901aecdc700de
Eugeny Okhrimenko <Eugeny.Okhrimenko@virtoworks.com>
2025-04-28 19:40:33 +0200

    VCST-3168: Add middle name to address

--- src/VirtoCommerce.CustomerModule.Data.MySql/Migrations/20250428110549_AddAddressMiddleName.Designer.cs
+++ src/VirtoCommerce.CustomerModule.Data.MySql/Migrations/20250428110549_AddAddressMiddleName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VirtoCommerce.CustomerModule.Data.Repositories;
 
@@ -11,9 +12,11 @@ using VirtoCommerce.CustomerModule.Data.Repositories;
 namespace VirtoCommerce.CustomerModule.Data.MySql.Migrations
 {
     [DbContext(typeof(CustomerDbContext))]
-    partial class CustomerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250428110549_AddAddressMiddleName")]
+    partial class AddAddressMiddleName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
