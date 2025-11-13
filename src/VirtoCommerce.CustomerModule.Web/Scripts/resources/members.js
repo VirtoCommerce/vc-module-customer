5d30070c86bdb2654356a30f1f166cd836905a3d
Basil Kotov <basilkot@gmail.com>
2025-11-13 07:54:48 +0000

    VCST-4221: Fix birthday displaying (#284)

--- src/VirtoCommerce.CustomerModule.Web/Scripts/resources/members.js
+++ src/VirtoCommerce.CustomerModule.Web/Scripts/resources/members.js
@@ -3,6 +3,15 @@ angular.module('virtoCommerce.customerModule')
     return $resource('api/members/:id', {}, {
         search: { method: 'POST', url: 'api/members/search' },
         update: { method: 'PUT' },
+        get: {
+            transformResponse: function (data) {
+                const result = angular.fromJson(data);
+                if (result.birthDate === '0001-01-01T00:00:00Z') {
+                    result.birthDate = null;
+                }
+                return result;
+            }
+        },
         delete: { method: 'POST', url: 'api/members/delete' },
         getByUserId: { method: 'GET', url: 'api/members/accounts/:userId' }
     });
